d1e54fa83d5e7f942c1fbaad2161fe05e42e3a11
Owen <woramet.le@ku.th>
2026-09-22 00:39:52 +0700

    feat(ui): implement TableScreen and update App component structure

---
+++ src/screens/TableScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+import { styles } from '../styles/tableStyle'
+
+export default function TableScreen() {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.header}>หน้าเลือกโต๊ะ</Text>
+        </View>
+)}
