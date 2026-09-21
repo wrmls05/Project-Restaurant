eb56035c2cb81d05788826abdf918a8a729f6210
Owen <woramet.le@ku.th>
2026-09-22 00:41:25 +0700

    fix(TableScreen): format return statement and add newline at end of file

--- src/screens/TableScreen.js
+++ src/screens/TableScreen.js
@@ -3,8 +3,9 @@ import { View, Text } from "react-native";
 import { styles } from '../styles/tableStyle'
 
 export default function TableScreen() {
-    return(
+    return (
         <View style={styles.container}>
             <Text style={styles.header}>หน้าเลือกโต๊ะ</Text>
         </View>
-)}
+    )
+}
